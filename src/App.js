5d91b21dedab4a10ca3327f12f4094bed45fdd9f
Dipak Chavda <dipakchavda9@gmail.com>
2022-01-30 11:38:47 +0530

    Changed app text.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!
+          Hi there! how are you?
         </p>
         <a
           className="App-link"
